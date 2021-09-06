f163b966f9ae85111403c2a4aeaaef4fe3688b6e
Eric Moynihan <git@moynihan.io>
2021-09-06 17:35:29 +0100

    Entrypoint to ssh into control host

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,17 @@
 #!/bin/sh -l
 
-echo "hello $1"
+echo "$1" > id_rsa
+
+ssh -i id_rsa $2@$3 '
+	cd $4
+	git pull
+	export VAULT_PASS=$5
+
+	virtualenv -p /usr/bin/python3 .
+	source bin/activate
+	pip install -r ./requirements.txt
+	ansible-galaxy install -r requirements.yml
+	source proxmox_secrets.sh
+
+	./run.sh $6 --tags $7
+'
