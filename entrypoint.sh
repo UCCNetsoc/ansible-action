171f5f4abe35395aa386ed33cf82b830e170e806
Eric Moynihan <git@moynihan.io>
2021-09-07 00:17:13 +0100

    Added comments to action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,27 @@
 
 echo "$1" > id_rsa
 
+echo "SSHing into control host."
+
 ssh -q -o StrictHostKeyChecking=no -o GlobalKnownHostsFile=/dev/null -o UserKnownHostsFile=/dev/null -i id_rsa -p $8 $2@$3 '
 	cd $4
 	export VAULT_PASS=$5
 
 	if [[ "$(git pull)" != *"Already up to date."* ]]; then
-		echo "Updated repo, cloning"
+		echo "Updated repo, cloning."
 		virtualenv -p /usr/bin/python3 .
 		source bin/activate
 		pip install -r ./requirements.txt
 		ansible-galaxy install -r requirements.yml
 	fi
 
-	echo "Setting proxmox secrets"
+	echo "Setting proxmox secrets."
 	source bin/activate
 	source proxmox_secrets.sh
 
-	echo "Running playbook"
+	echo "Running playbook."
 	echo $5 > ./_vault_pass
         ansible-playbook -i proxmox_inventory.py -i hosts --vault-password-file ./_vault_pass $6 --tags $7
 '
+
+echo "Execution finished"
